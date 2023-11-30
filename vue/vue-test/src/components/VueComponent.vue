a2ed4db812d64e48505f60b22d0bd9633a49c457
heejaeRA <rhj7369@nate.com>
2023-11-30 14:06:16 +0900

    vue

---
+++ vue/vue-test/src/components/VueComponent.vue
@@ -0,0 +1,24 @@
+<template>
+	<div>
+		<label><input type="text" v-model="msg" /></label>
+		<p>{{ msg }}</p>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'VueComponent',
+	data() {
+		return {
+			msg: '',
+		};
+	},
+};
+</script>
+
+<style>
+p {
+	background-color: papayawhip;
+	text-align: center;
+}
+</style>
