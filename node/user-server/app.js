000745129782599e78e5bc8c1ce34acdc1b7fbe8
heejaeRA <rhj7369@nate.com>
2023-12-06 10:31:00 +0900

    빌드

--- node/user-server/app.js
+++ node/user-server/app.js
@@ -5,32 +5,38 @@ const mysql = require('./db');
 
 app.use(express.json({ limit: '50mb' }));
 
+app.use(express.static('dist'));
+
 app.listen(3001, () => {
 	console.log('서버 시작');
 });
 
-app.get('/users', async (req, resp) => {
+app.get('/', (req, resp) => {
+	resp.sendFile('/dist/index.html');
+});
+
+app.get('/node/users', async (req, resp) => {
 	let result = await mysql.query('userList');
 	resp.send(result);
 });
 
-app.get('/users/:userNo', async (req, resp) => {
+app.get('/node/users/:userNo', async (req, resp) => {
 	let result = await mysql.query('userInfo', req.params.userNo);
 	resp.send(result[0]);
 });
 
-app.post('/users', async (req, resp) => {
+app.post('/node/users', async (req, resp) => {
 	let result = await mysql.query('userInsert', req.body.data);
 	resp.send(result);
 });
 
-app.put('/users/:userNo', async (req, resp) => {
+app.put('/node/users/:userNo', async (req, resp) => {
 	let data = [req.body.data, req.params.userNo];
 	let result = await mysql.query('userUpdate', data);
 	resp.send(result);
 });
 
-app.delete('/users/:userNo', async (req, resp) => {
+app.delete('/node/users/:userNo', async (req, resp) => {
 	let result = await mysql.query('userDelete', req.params.userNo);
 	resp.send(result);
 });
